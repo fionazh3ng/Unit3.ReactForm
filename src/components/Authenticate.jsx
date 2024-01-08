0b249111184e28f77de3f66109ef25f207580afe
Fiona Zheng <fionazh3ng@fionas-mbp.mynetworksettings.com>
2024-01-08 17:16:51 -0500

    finished workshop

---
+++ src/components/Authenticate.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const Authenticate = ({ token }) => {
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/authenticate",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const result = await response.json();
+      setSuccessMessage(result.message);
+      const name = setSuccessMessage(result.data.username);
+      console.log(result.message);
+      setError("");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  return (
+    <>
+      <h2>Authenticate</h2>
+      {successMessage && <p>{successMessage}</p>}
+      {error && <p>{error}</p>}
+      <button onClick={handleClick}>Authenticate Token</button>
+    </>
+  );
+};
+
+export default Authenticate;
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+const SignUpForm = ({ setToken }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: "fifister",
+            password: "super-secret-fifi",
+          }),
+        }
+      );
+      const result = await response.json();
+      if (username.length < 8) {
+        setError(
+          "Invalid input, username must contain at least 8 characters. Please try again."
+        );
+        result.success = false;
+        setToken(null);
+      } else {
+        console.log(result);
+        setToken(result.token);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <>
+      <h2>Sign Up</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button>Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default SignUpForm;
